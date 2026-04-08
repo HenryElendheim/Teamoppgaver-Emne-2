655c5a4253a9fd4dfe161f13c9245bfd0a0cee24
HenryElendheim <henskielendheim@gmail.com>
2026-04-08 14:35:01 +0200

    guygyu

--- Geir's Musikkbibliotek/View/Add_Or_Edit_New_Music/view.js
+++ Geir's Musikkbibliotek/View/Add_Or_Edit_New_Music/view.js
@@ -1,12 +1,31 @@
 function editDetailsPage() {
     return /*HTML*/ `
-    <input onchange="setArtist(this.value)">
+    <input onchange="setInfo(this.value)" placeholder="Artist navn">
+    <input onchange="setInfo(this.value)" placeholder="Album/Singel/EP">
+    <input type="checkbox">Stue
+    <input type="checkbox">Loft
+    <input onchange="setInfo(this.value)" placeholder="Release Year">
+    <button onclick="submitChanges()">Lagre</button>
 
     `;
 }
 
-function setArtist(artistName) {
-    model.viewState.musicInfo.artist = artistName;
-    model.data.musicInfo.push(artistName);
+
+function setArtist() {
+    model.data.musicInfo = artistName;
+}
+
+
+function setInfo(artistName, albumName, location, releaseYear) {
+    model.data.musicInfo.push({
+        artist: artistName,
+        album: albumName,
+        lokasjon: location,
+        yearOfRelease: releaseYear,
+    });
+}
+
+
+function submitChanges() {
     updateView();
 }
