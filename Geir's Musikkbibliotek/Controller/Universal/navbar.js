595f21254132a4fdc617bd6b5977d66d67ab6dea
HenryElendheim <henskielendheim@gmail.com>
2026-04-15 14:17:27 +0200

    rwewerwe

---
+++ Geir's Musikkbibliotek/Controller/Universal/navbar.js
@@ -0,0 +1,31 @@
+function toggleMobileMenu() {
+    const menu = document.getElementById('mobile-menu');
+
+    // Selve togglen
+    model.app.mobileMenuToggle = !model.app.mobileMenuToggle;
+
+    //forkortelse
+    const mobileMenu = model.app.mobileMenuToggle;
+
+    if (mobileMenu == !mobileMenu) {
+        menu.classList.toggle('open');
+        console.log(mobileMenu)
+    } else {
+        menu.classList.remove('open');
+        console.log(mobileMenu)
+    }
+
+}
+
+// function navbar() {
+//     return /*HTML*/ `
+//         <button onclick="changePage('homePage')">Hjem</button>
+//         <button onclick="changePage('wishList')">Ønskeliste</button>
+
+//         <input id="navSearchInput" placeholder="Søkefelt" oninput="model.viewState.searchBar = this.value">
+//         <button onclick="changePage('searchPage')">Søk</button>
+
+//         <button onclick="changePage('login')">Logg inn</button>
+//         <button onclick="changePage('profile')">Profil</button>
+//         `;
+// }
