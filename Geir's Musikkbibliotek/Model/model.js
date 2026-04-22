219ff1b69642d64138bac4caa972013cd7464dc6
HenryElendheim <henskielendheim@gmail.com>
2026-04-22 12:40:28 +0200

    utyuyt

--- Geir's Musikkbibliotek/Model/model.js
+++ Geir's Musikkbibliotek/Model/model.js
@@ -63,56 +63,83 @@ const model = {
 
 		musicInfo: [
 			{
-				id: 1,
-				title: "Thriller",
-				artist: "Michael Jackson",
-				location: [0],
-				releaseYear: 1982,
-				genre: [3],
-				notes:
-					"As the best-selling album in history (estimated over 70 million copies), it revolutionized music videos and broke racial barriers in pop music.",
-				wishlist: false,
-				coverImg: (href =
-					"https://www.platekompaniet.no/pimcorecdn/Products/Music/LP/LPF/image-thumb__605110__Image800/1740380669.jpg?auto=webp&format=pjpg&width=160&height=160&fit=cover,"),
+				userId: 1,
+				musicColletcion: [
+					{
+						id: 1,
+						title: "Thriller",
+						artist: "Michael Jackson",
+						location: [0],
+						releaseYear: 1982,
+						genre: [3],
+						notes:
+							"As the best-selling album in history (estimated over 70 million copies), it revolutionized music videos and broke racial barriers in pop music.",
+						wishlist: false,
+						coverImg: (href =
+							"https://www.platekompaniet.no/pimcorecdn/Products/Music/LP/LPF/image-thumb__605110__Image800/1740380669.jpg?auto=webp&format=pjpg&width=160&height=160&fit=cover,"),
+					},
+					{
+						id: 2,
+						title: "The Dark Side of the Moon",
+						artist: "Pink Floyd",
+						location: [2],
+						releaseYear: 1973,
+						genre: [0],
+						notes:
+							"Widely praised for its sonic production, thematic cohesion, and massive popularity, it is regarded as a perfect rock album.",
+						wishlist: false,
+						coverImg: (href =
+							"https://imusic.b-cdn.net/images/item/original/024/0196587714024.jpg?pink-floyd-2025-the-dark-side-of-the-moon-50th-anniversary-edition-cd&class=original&v=1739720514"),
+					},
+					{
+						id: 3,
+						title: "Abbey Road",
+						artist: "The Beatles",
+						location: [1],
+						releaseYear: 1969,
+						genre: [0],
+						notes:
+							"Often cited in critical polls as one of the best-produced and most influential albums in music history.",
+						wishlist: false,
+						coverImg: (href =
+							"https://imusic.b-cdn.net/images/item/original/439/0602508007439.jpg?the-beatles-2019-abbey-road-50th-anniversary-cd&class=original&v=1565271105"),
+					},
+				]
 			},
 			{
-				id: 2,
-				title: "The Dark Side of the Moon",
-				artist: "Pink Floyd",
-				location: [2],
-				releaseYear: 1973,
-				genre: [0],
-				notes:
-					"Widely praised for its sonic production, thematic cohesion, and massive popularity, it is regarded as a perfect rock album.",
-				wishlist: false,
-				coverImg: (href =
-					"https://imusic.b-cdn.net/images/item/original/024/0196587714024.jpg?pink-floyd-2025-the-dark-side-of-the-moon-50th-anniversary-edition-cd&class=original&v=1739720514"),
-			},
-			{
-				id: 3,
-				title: "Abbey Road",
-				artist: "The Beatles",
-				location: [1],
-				releaseYear: 1969,
-				genre: [0],
-				notes:
-					"Often cited in critical polls as one of the best-produced and most influential albums in music history.",
-				wishlist: false,
-				coverImg: (href =
-					"https://imusic.b-cdn.net/images/item/original/439/0602508007439.jpg?the-beatles-2019-abbey-road-50th-anniversary-cd&class=original&v=1565271105"),
+				userId: 2,
+				musicColletcion: [
+					{
+						id: 1,
+						title: "Thriller",
+						artist: "Michael Jackson",
+						location: [0],
+						releaseYear: 1982,
+						genre: [3],
+						notes:
+							"As the best-selling album in history (estimated over 70 million copies), it revolutionized music videos and broke racial barriers in pop music.",
+						wishlist: false,
+						coverImg: (href =
+							"https://www.platekompaniet.no/pimcorecdn/Products/Music/LP/LPF/image-thumb__605110__Image800/1740380669.jpg?auto=webp&format=pjpg&width=160&height=160&fit=cover,"),
+					},
+				]
 			},
 		],
 
 		users: [
 			{
 				id: 1,
-				username: "Geir",
-				password: "geir",
+				username: "Admin",
+				password: "geir123",
 			},
 			{
 				id: 2,
-				username: "Geir Andre",
-				password: "geir2",
+				username: "Gjest",
+			},
+			{
+				id: 3,
+				username: "FAB", //Foreldre administrert bruker
+				password: "123",
 			},
 		],
 	},
